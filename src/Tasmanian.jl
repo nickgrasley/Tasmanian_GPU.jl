cd6090f44b490b96d10f9d502701c2ec555bbb60
MichelJuillard <michel.juillard@mjui.fr>
2024-07-29 16:41:34 +0200

    fix exports

--- src/Tasmanian.jl
+++ src/Tasmanian.jl
@@ -1,6 +1,7 @@
 module Tasmanian
 
 import Base: show, read, run, write
+using LinearAlgebra
 using Plots
 using Random
 using SparseArrays
@@ -13,10 +14,22 @@ include("libTasmanian.jl")
 include("TSG.jl")
 include("../examples/examples.jl")
 
-export TasmanianSG, LocalRules, copyGrid, evaluateBatch, evaluateBatch!, getDims,
-getLoadedPoints, getNeededPoints, getNout, getNumDimensions, getNumLoaded,
-getNumNeeded, getNumOutputs, getNumPoints, getOrder, getPoints, isFourier,
-isGlobal, isLocalPolynomial, isSequence, isWavelet, loadNeededPoints!,
-makeLocalPolynomialGrid!, setDomainTransform!, setSurplusRefinement!, differentiate!
+export TasmanianSG, LocalRules, compareGrids, copyGrid, differentiate!, enableAcceleration,
+    estimateAnisotropicCoefficients, evaluate, evaluateBatch,
+    evaluateBatch!, evaluateHierarchicalFunctions, evaluateSparseHierarchicalFunctions,
+    evaluateThreadSafe, getAlpha, getBeta, getConformalTransformASIN, getDims,
+    getDomainTransform, getHierarchicalCoefficients,
+    getInterpolationWeights, getInterpolationWeightsBatch, getGPUMemory, getLevelLimits, getLoadedPoints,
+    getNeededPoints, getNout, getNumDimensions, getGPUName, getNumGPUs, getNumLoaded,
+    getNumNeeded, getNumOutputs, getNumPoints, getOrder, getPoints,
+    getQuadratureWeights, integrate, isAccelerationAvailable, isFourier, isGlobal,
+    isLocalPolynomial, isSequence, isSetDomainTransform, isSetConformalTransformASIN,
+    isWavelet,
+    loadNeededPoints!, loadNeededValues!, makeFourierGrid!, makeGlobalGrid!,
+    makeLocalPolynomialGrid!, makeSequenceGrid!, removePointsByHierarchicalCoefficient!, setAnisotropicRefinement!,
+    setConformalTransformASIN!, makeWaveletGrid!,
+    setDomainTransform!, setGPUID!, setHierarchicalCoefficients!, setSurplusRefinement!, updateFourierGrid!, updateGlobalGrid!,
+    updateLocalPolynomialGrid!, updateSequenceGrid!, updateWaveletGrid!
+
 
 end # module

f2ba6cfcd7d37611e4cb05d864cf43a4a2812ad7
MichelJuillard <michel.juillard@mjui.fr>
2024-07-29 16:42:15 +0200

    fix test

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,7 +8,7 @@ using Test
     out = 1
     depth = 5
     tsg = Tasmanian.TasmanianSG(dim, out, depth)
-    @test tsg.version == VersionNumber("8.0")
+    @test Tasmanian.getVersion() == VersionNumber("8.0")
     Tasmanian.makeLocalPolynomialGrid!(tsg)
     @test Tasmanian.isLocalPolynomial(tsg)
     @test !Tasmanian.isGlobal(tsg)
